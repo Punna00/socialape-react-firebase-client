e6c9939cffa415e9117581d0ace5104277c11c9d
Punna iMAC <punna104268@gmail.com>
2020-07-22 19:37:02 +0700

    Using Day.js displays dates and times

--- src/components/Scream.js
+++ src/components/Scream.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import withStyles from '@material-ui/core/styles/withStyles';
 import { Link } from 'react-router-dom';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
 
 // MUI Stuff
 import Card from '@material-ui/core/Card';
@@ -24,6 +26,7 @@ const styles = {
 
 class Scream extends Component {
   render() {
+    dayjs.extend(relativeTime)
     const {
       classes,
       scream: {
@@ -42,7 +45,7 @@ class Scream extends Component {
         <CardMedia image={userImage} title="Profile image" className={classes.image} />
         <CardContent className={classes.content}>
           <Typography variant="h5" component={Link} to={`/users/${userHandle}`} color="primary">{userHandle}</Typography>
-          <Typography variant="body2" color="textSecondary">{createdAt}</Typography>
+          <Typography variant="body2" color="textSecondary">{dayjs(createdAt).fromNow()}</Typography>
           <Typography variant="body1">{body}</Typography>
         </CardContent>
       </Card>
