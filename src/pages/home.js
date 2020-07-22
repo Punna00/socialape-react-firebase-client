ecd4e5e095e037939e11726c3e437bd094b6ab45
Punna iMAC <punna104268@gmail.com>
2020-07-22 16:07:21 +0700

    fix bug

--- src/pages/home.js
+++ src/pages/home.js
@@ -11,7 +11,7 @@ class home extends Component {
   componentDidMount() {
     axios.get('/screams')
       .then(res => {
-        console.log(res.data)
+        //console.log(res.data)
         this.setState({
           screams: res.data
         })
@@ -20,7 +20,7 @@ class home extends Component {
   }
   render() {
     let recentScreamsMarkup = this.state.screams ? (
-      this.state.screams.map((scream) => <Scream scream={scream} />)
+      this.state.screams.map((scream) => <Scream key={scream.screamId} scream={scream} />)
     ) : (<p>Loading...</p>)
     return (
       <Grid container spacing={10}>
