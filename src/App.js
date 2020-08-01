7da1233796f06e58b49b8c5e5973c55b4e0fedb4
Punna00 <poko-nyan@hotmail.com>
2020-08-01 20:00:37 +0700

    Update path

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import store from './redux/store';
 import { SET_AUTHENTICATED } from './redux/types';
 import { logoutUser, getUserData } from './redux/actions/userAction';
 // Components
-import Navbar from './components/Navbar';
+import Navbar from './components/layout/Navbar';
 import AuthRoute from './util/AuthRoute';
 import themeFile from './util/theme';
 // Pages
--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 import withStyles from '@material-ui/core/styles/withStyles';
 import { Link } from 'react-router-dom';
 import dayjs from 'dayjs';
-import EditDetails from '../components/EditDetails';
-import MyButton from '../util/MyButton';
+import EditDetails from './EditDetails';
+import MyButton from '../../util/MyButton';
 // MUI stuff
 import Button from '@material-ui/core/Button';
 import MuiLink from '@material-ui/core/Link';
@@ -18,7 +18,7 @@ import EditIcon from '@material-ui/icons/Edit';
 import KeyboardReturn from '@material-ui/icons/KeyboardReturn';
 // Redux
 import { connect } from 'react-redux';
-import { logoutUser, uploadImage } from '../redux/actions/userAction';
+import { logoutUser, uploadImage } from '../../redux/actions/userAction';
 
 
 const styles = (theme) => ({
